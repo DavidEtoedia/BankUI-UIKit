e65ea4c9b9f8c99780570a1602345cee2f004d0e
DavidEtoedia <etoedia@gmail.com>
2024-09-09 15:10:01 -0700

    Initial Commit

---
+++ BankUI-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankUI-UIKit
+//
+//  Created by Inyene Etoedia on 09/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
